e61737e7a5797bd64949b6e5795ae2df2e8d5d2b
botARPIT <arpitchauhanofficial>
2025-10-16 20:04:56 +0530

    learnt to build simple chain

---
+++ chains/simple_chain.py
@@ -0,0 +1,24 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id = "mistralai/Mistral-7B-Instruct-v0.2",
+    # repo_id="google/gemma-2-2b-it",
+    task = "text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+template = PromptTemplate(
+    template = "Write 4 lines about the following topic \n {topic}",
+    input_variables=["topic"] 
+)
+
+parser = StrOutputParser()
+chain = template | model | parser
+response = chain.invoke({"Democracy in china"})
+print(response)
+chain.get_graph().print_ascii()
